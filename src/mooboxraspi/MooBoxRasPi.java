10e3d557c1620b63a070c18c8888a76067f53ea2
Darksidious <master.darksidious@gmail.com>
2015-03-19 08:48:40 +0100

    Adding halt and catchfire anim

--- src/mooboxraspi/MooBoxRasPi.java
+++ src/mooboxraspi/MooBoxRasPi.java
@@ -5,30 +5,27 @@
  */
 package mooboxraspi;
 
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
-import com.pi4j.io.gpio.PinPullResistance;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.*;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import com.pi4j.wiringpi.Gpio;
 import com.pi4j.wiringpi.SoftPwm;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author darksidious
  */
 public class MooBoxRasPi {
 
     public static final String SERVO_MOO_POSITION = "servo.moo.position";
     public static final String SERVO_SLEEP_POSITION = "servo.sleep.position";
+    public static final String SHUTDOWN_AFTER_MS = "shutdown.after.ms";
     public static final String SERVER_PORT = "server.port";
     private static double frHz = 100;
     private static double leftP = 0.5;
@@ -39,13 +36,15 @@ public class MooBoxRasPi {
 
     private static final double msPerCycle = 1000 / frHz;
 
+    private static int shutdownAfter = 3000;
+    private static long stateLowStarted = 0;
+
     private static final double positionMeuuuuh = leftP * msPerCycle;
     private static final double posistionDeReposInitial = rightP * msPerCycle;
 
 
-
     private static GpioController gpio;
-    
+
     public static final Logger logger = Logger.getLogger(MooBoxRasPi.class.getName());
     public static PinSignalThread animServo1;
     public static PinSignalThread animServo2;
@@ -61,8 +60,10 @@ public class MooBoxRasPi {
         Gpio.wiringPiSetup();
         gpio = GpioFactory.getInstance();
 
+        shutdownAfter = Config.getPropertyAsInt(SHUTDOWN_AFTER_MS, shutdownAfter);
         leftP = Config.getPropertyAsDouble(SERVO_MOO_POSITION, leftP);
         rightP = Config.getPropertyAsDouble(SERVO_SLEEP_POSITION, rightP);
+
 
         initServo();
         startAnim();
@@ -71,8 +72,8 @@ public class MooBoxRasPi {
         initServer();
 
         // keep program running until user aborts (CTRL-C)
-        while(true)
-        {}
+        while (true) {
+        }
     }
 
     private static GpioPinListenerDigital initButtonServoAnimListner(PinSignalThread runnable) {
@@ -117,46 +118,69 @@ public class MooBoxRasPi {
 
         final GpioPinDigitalInput servoButton2 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04, PinPullResistance.PULL_UP);
 
-        animServo1 = new PinSignalThread(positionMeuuuuh, posistionDeReposInitial, GPIO0,"#GREENMOO","greenMoo.sh");
-        animServo2 = new PinSignalThread(positionMeuuuuh, posistionDeReposInitial, GPIO1,"#REDMOO", "redMoo.sh");
+        animServo1 = new PinSignalThread(positionMeuuuuh, posistionDeReposInitial, GPIO0, "#GREENMOO", "greenMoo.sh");
+        animServo2 = new PinSignalThread(positionMeuuuuh, posistionDeReposInitial, GPIO1, "#REDMOO", "redMoo.sh");
 
         servoButton.addListener(initButtonServoAnimListner(animServo1));
         servoButton2.addListener(initButtonServoAnimListner(animServo2));
-        
-        
+
+
         final GpioPinDigitalInput shutdownButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_13, PinPullResistance.PULL_UP);
 
         shutdownButton.addListener(initButtonShutdownListner());
     }
-    
-        private static GpioPinListenerDigital initButtonShutdownListner() {
+
+    private static GpioPinListenerDigital initButtonShutdownListner() {
         return (GpioPinDigitalStateChangeEvent event) -> {
             logger.log(Level.INFO, " --> GPIO PIN STATE CHANGE: {0} = {1}", new Object[]{event.getPin(), event.getState()});
-            if (event.getState().equals(PinState.LOW)) {
-                Thread thread = new Thread(() -> {
-                    try {
-                        logger.info("let's try to send a mail...");
-                        String[] commands = new String[]{"/opt/MooBoxProject/scripts/sleep.sh"};
-                        Runtime.getRuntime().exec(commands);
-                        Runtime.getRuntime().exec("halt");
-                    }
-                    catch (IOException e) {
-                        logger.log(Level.SEVERE, null, e);
-                    }
-                });
+            if (event.getState().equals(PinState.HIGH)) {
+                if (stateLowStarted != 0) {
+                    if (new Date().getTime() > (stateLowStarted + shutdownAfter)) {
+                        Thread thread = new Thread(() -> {
+                            try {
+                                logger.info("executing halt sleep...");
+                                String[] commands = new String[]{"/opt/MooBoxProject/scripts/sleep.sh"};
+                                Runtime.getRuntime().exec(commands);
+                                Runtime.getRuntime().exec("halt");
+                            }
+                            catch (IOException e) {
+                                logger.log(Level.SEVERE, null, e);
+                            }
+                        });
 
-                thread.start();
+                        thread.start();
+                    }else if(new Date().getTime() > (stateLowStarted + 10)){
+                        Thread thread = new Thread(() -> {
+                            try {
+                                logger.info("executing halt and catch fire...");
+                                String[] commands = new String[]{"/opt/MooBoxProject/scripts/hcf.sh"};
+                                Runtime.getRuntime().exec(commands);
+                            }
+                            catch (IOException e) {
+                                logger.log(Level.SEVERE, null, e);
+                            }
+                        });
+
+                        thread.start();
+                    }
+                }
+
+                stateLowStarted = 0;
+            }
+            if (event.getState().equals(PinState.LOW)) {
+                stateLowStarted = new Date().getTime();
+
             }
         };
     }
 
-    private static int port=8091, maxConnections=0;
+    private static int port = 8091, maxConnections = 0;
 
-    private static void initServer(){
-        port =  Config.getPropertyAsInt(SERVER_PORT, port);
-        int i=0;
+    private static void initServer() {
+        port = Config.getPropertyAsInt(SERVER_PORT, port);
+        int i = 0;
 
-        try{
+        try {
             ServerSocket listener = null;
             try {
                 listener = new ServerSocket(port);
@@ -166,15 +190,16 @@ public class MooBoxRasPi {
             }
             Socket server;
 
-            while((i++ < maxConnections) || (maxConnections == 0)){
+            while ((i++ < maxConnections) || (maxConnections == 0)) {
                 MooBoxRunnableServer connection;
 
                 server = listener.accept();
-                MooBoxRunnableServer conn_c= new MooBoxRunnableServer(server);
+                MooBoxRunnableServer conn_c = new MooBoxRunnableServer(server);
                 Thread t = new Thread(conn_c);
                 t.start();
             }
-        } catch (IOException ioe) {
+        }
+        catch (IOException ioe) {
             logger.severe("IOException on socket listen: " + ioe);
         }
     }
